90679128fd8dd25b1859a2d7de04df5d03da1d2c
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-06-18 16:50:01 -0300

    chore: Day 02

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
+import 'reflect-metadata';
 import express from 'express';
+
+import { router } from './routes';
+
+import './database';
 
 const app = express();
 
-app.get('/test', (request, response) => {
-  return response.send('Olá NLW');
-});
-
-app.post('/test-post', (request, response) => {
-  return response.send('Olá NLW método POST');
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3000, () => console.log('Server is running'));
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,38 @@
+import { getCustomRepository } from 'typeorm';
+import { UsersRepositories } from '../repositories/UsersRepositories';
+
+interface IUseRequest {
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+
+class CreateUserService {
+  async execute({ name, email, admin}: IUseRequest) {
+    const usersRepositories = getCustomRepository(UsersRepositories)
+
+    if (!email) {
+      throw new Error('Email incorrect');
+    }
+
+    const userAlreadyExists = await usersRepositories.findOne({
+      email
+    });
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    const user = usersRepositories.create({
+      name,
+      email,
+      admin
+    });
+
+    await usersRepositories.save(user);
+
+    return user;
+  }
+}
+
+export { CreateUserService };
